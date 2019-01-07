b885147bc37c4d26b645c589bb29ab66fa8706b7
Yong SHI <shiyong212@gmail.com>
2019-01-07 22:12:27 +0800

    Rotate List

---
+++ Rotate List.py
@@ -0,0 +1,58 @@
+'''
+Rotate List
+  Go to Discuss
+Given a linked list, rotate the list to the right by k places, where k is non-negative.
+
+Example 1:
+
+Input: 1->2->3->4->5->NULL, k = 2
+Output: 4->5->1->2->3->NULL
+Explanation:
+rotate 1 steps to the right: 5->1->2->3->4->NULL
+rotate 2 steps to the right: 4->5->1->2->3->NULL
+Example 2:
+
+Input: 0->1->2->NULL, k = 4
+Output: 2->0->1->NULL
+Explanation:
+rotate 1 steps to the right: 2->0->1->NULL
+rotate 2 steps to the right: 1->2->0->NULL
+rotate 3 steps to the right: 0->1->2->NULL
+rotate 4 steps to the right: 2->0->1->NULL
+'''
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def rotateRight(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        
+        # add nodes of link to list
+        list = []
+        p = head
+        while p:
+            list.append(p)
+            p = p.next
+        # rotate list to right by k places, k%size
+        k = k%len(list)
+        list = list[-k:] + list[:-k]
+
+        # link nodes in new list
+        pre = dummy = ListNode(-1)
+        for p in list:
+            pre.next = p            
+            pre = p
+            
+        # last node point to None
+        pre.next = None
+        
+        return dummy.next
